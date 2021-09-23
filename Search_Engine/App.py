ea84a7c1144ba8f9563c9529db75c935f957628b
mjacoupy <maxime.jacoupy@gmail.com>
2021-09-23 14:35:22 +0200

    without expand

--- Search_Engine/App.py
+++ Search_Engine/App.py
@@ -335,7 +335,7 @@ if analysis == "[3] Search Engine":
                 # print the selected previews
                 st.markdown("___"+str(iCpt+1)+". "+txt+"___")
                 st.markdown(final_content)
-                with st.expander("See original page"):
-                    img = Image.open(os.path.join("ocr_exports", "se_png", sel_png))
-                    st.image(img)
+                # with st.expander("See original page"):
+                #     img = Image.open(os.path.join("ocr_exports", "se_png", sel_png))
+                #     st.image(img)
                 st.markdown("""---""")
