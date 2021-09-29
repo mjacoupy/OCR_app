efada359d1dda7fc7119f7eb38a14e931ff281b4
mjacoupy <maxime.jacoupy@gmail.com>
2021-09-29 11:48:03 +0200

    update

--- Search_Engine/PG-App.py
+++ Search_Engine/PG-App.py
@@ -2,16 +2,16 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-09-29 11:47:05
+# @Last Modified time: 2021-09-29 11:47:55
 
 import streamlit as st
 import s3fs
 from PIL import Image
-import cv2
 import numpy as np
 
 fs = s3fs.S3FileSystem(anon=False)
 content = "ocrplus-ptc/Page_6.jpeg"
+
 
 @st.cache(ttl=600)
 def read_file(filename):
@@ -19,7 +19,7 @@ def read_file(filename):
 
     infile = fs.open(filename, "rb")
     pil_image = Image.open(infile).convert('RGB')
-    open_cv_image = numpy.array(pil_image)
+    open_cv_image = np.array(pil_image)
     image = open_cv_image[:, :, ::-1].copy()
     return image
 
@@ -30,4 +30,3 @@ image = read_file(content)
 st.image(image, caption="first test")
 
 st.text(image)
-
