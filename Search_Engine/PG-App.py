615b1a3583036f771f595b647047d8a84557deb5
mjacoupy <maxime.jacoupy@gmail.com>
2021-09-29 13:48:11 +0200

    connexion to streamlitbucketmja

--- Search_Engine/PG-App.py
+++ Search_Engine/PG-App.py
@@ -2,7 +2,7 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-09-29 12:55:48
+# @Last Modified time: 2021-09-29 13:48:02
 
 import streamlit as st
 import s3fs
@@ -10,8 +10,9 @@ from PIL import Image
 import numpy as np
 
 fs = s3fs.S3FileSystem(anon=False)
-# content = "ocrplus-ptc/Page_6.jpeg"
-content = "ocrplus-app-mja/03_ARDIAN_P4.jpeg"
+content = "ocrplus-ptc/Page_6.jpeg"
+# content = "ocrplus-app-mja/03_ARDIAN_P4.jpeg"
+
 
 @st.cache(ttl=600)
 def read_file(filename):
