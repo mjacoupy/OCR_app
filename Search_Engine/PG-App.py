0999b5ff1f87c913730828a016b0bd085c74d08c
mjacoupy <maxime.jacoupy@gmail.com>
2021-10-01 10:21:53 +0200

    test

--- Search_Engine/PG-App.py
+++ Search_Engine/PG-App.py
@@ -2,7 +2,7 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-10-01 10:18:01
+# @Last Modified time: 2021-10-01 10:21:49
 
 
 # #######################################################################################################################
@@ -148,7 +148,9 @@ if analysis == "[1] Image Import":
         is_success, im_buf_arr = cv2.imencode(".jpg", im)
         byte_im = im_buf_arr.tobytes()
 
-        st.markdown(byte_im)
+        a = byte_im.copy()
+        st.markdown(a)
+
         #s3.Bucket(my_bucket).put_object(Key='test_image.png', Body=byte_im)
 
 # #######################################################################################################################
