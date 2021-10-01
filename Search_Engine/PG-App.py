c60479aee8001b9d92497a30b9cd455692e33066
mjacoupy <maxime.jacoupy@gmail.com>
2021-10-01 10:35:18 +0200

    test

--- Search_Engine/PG-App.py
+++ Search_Engine/PG-App.py
@@ -2,7 +2,7 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-10-01 10:34:39
+# @Last Modified time: 2021-10-01 10:35:12
 
 
 # #######################################################################################################################
@@ -149,7 +149,7 @@ if analysis == "[1] Image Import":
 
         st.text(img_str)
 
-        #s3.Bucket(my_bucket).put_object(Key='test_image.png', Body=img_str)
+        s3.Bucket(my_bucket).put_object(Key='test_image.png', Body=img_str)
 
 # #######################################################################################################################
 #                                              # === PROCESS NEW FILE === #
