5a34b754618e26835f88a408ae039917950ca088
mjacoupy <maxime.jacoupy@gmail.com>
2021-10-01 15:05:16 +0200

    test

--- Search_Engine/PG-App.py
+++ Search_Engine/PG-App.py
@@ -2,7 +2,7 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-10-01 14:58:25
+# @Last Modified time: 2021-10-01 15:05:01
 
 
 # #######################################################################################################################
@@ -114,7 +114,7 @@ analysis = st.sidebar.selectbox('', ['[1] Image Import', '[2] Image Processing',
 # #######################################################################################################################
 #                                              # === IMPORT NEW FILE === #
 # #######################################################################################################################
-if analysis == "[1] Image Import":
+if analysis == "[1] Image Importxxxx":
     st.header('Image Import')
 
     side_bar()
@@ -157,17 +157,17 @@ if analysis == "[1] Image Import":
 # #######################################################################################################################
 #                                              # === IMPORT NEW FILE PDF === #
 # #######################################################################################################################
-# if analysis == "[1] Image Import":
-#     st.header('Image Import')
+if analysis == "[1] Image Import":
+    st.header('Image Import')
 
-#     side_bar()
+    side_bar()
 
-#     data = st.file_uploader("Upload a file", type=["pdf"])
-#     name = st.text_input('File name')
+    data = st.file_uploader("Upload a file", type=["pdf"])
+    name = st.text_input('File name')
 
-#     if data is not None:
-#         images = convert_from_bytes(data.read())
-#         int_val = st.slider('Page number', min_value=0, max_value=len(images), value=0, step=1)
+    if data is not None:
+        images = convert_from_bytes(data.read())
+        int_val = st.slider('Page number', min_value=0, max_value=len(images), value=0, step=1)
 
 
 
