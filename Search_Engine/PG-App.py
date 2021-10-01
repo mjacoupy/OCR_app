a517ae4f730fbaed4c7727b022424002ace4050a
mjacoupy <maxime.jacoupy@gmail.com>
2021-10-01 10:30:29 +0200

    test

--- Search_Engine/PG-App.py
+++ Search_Engine/PG-App.py
@@ -2,7 +2,7 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-10-01 10:28:57
+# @Last Modified time: 2021-10-01 10:30:20
 
 
 # #######################################################################################################################
@@ -148,7 +148,8 @@ if analysis == "[1] Image Import":
         with open(out_file, "rb") as image:
             f = image.read()
             b = bytearray(f)
-        st.text(b.type)
+            c = bytes(b)
+        st.text(c)
         #s3.Bucket(my_bucket).put_object(Key='test_image.png', Body=b)
 
 # #######################################################################################################################
