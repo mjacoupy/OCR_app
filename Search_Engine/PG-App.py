91d1e4667e074e960a4b88f9e2e7743858658d78
mjacoupy <maxime.jacoupy@gmail.com>
2021-10-01 13:58:00 +0200

    test

--- Search_Engine/PG-App.py
+++ Search_Engine/PG-App.py
@@ -2,7 +2,7 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-10-01 12:00:35
+# @Last Modified time: 2021-10-01 13:57:48
 
 
 # #######################################################################################################################
@@ -21,6 +21,7 @@ import pandas as pd
 from SearchEngine_app import SearchEngine
 import re
 import cv2
+import pdf2image
 
 # #######################################################################################################################
 #                                              # === S3 AWS === #
@@ -118,9 +119,8 @@ if analysis == "[1] Image Import":
     side_bar()
 
 
-    data = st.file_uploader("Upload a file", type=["png", "jpg", "jpeg"])
+    data = st.file_uploader("Upload a file", type=["png", "jpg", "jpeg", "pdf"])
     name = st.text_input('File name')
-
 
     if data is not None:
         image = Image.open(data)
@@ -128,26 +128,42 @@ if analysis == "[1] Image Import":
         open_cv_image = np.array(pil_image)
         image2 = open_cv_image[:, :, ::-1].copy()
 
-        scale_percent = 20
-        width = int(image2.shape[1] * scale_percent / 100)
-        height = int(image2.shape[0] * scale_percent / 100)
-        dim = (width, height)
-
-        # resize image
-        resized = cv2.resize(image2, dim, interpolation=cv2.INTER_AREA)
-
-        st.image(resized, caption='Selected document')
-
     button = st.button("Process")
 
+
     if data is not None and button:
-        export_path = os.path.join(os.path.abspath(os.getcwd()), "ocr_doc_to_process/")
-        out_file = export_path + str(name) + ".png"
-        cv2.imwrite(out_file, image2)
-        docs = os.listdir(export_path)
 
-        st.text('Done!')
+        if "pdf" in str(data.type):
+            images = pdf2image.convert_from_bytes(data.read())
 
+            int_val = st.slider('Page number', min_value=0, max_value=len(images), value=0, step=1)
+            button1 = st.button("Confirm page number")
+
+            if button1 and data is not None and button:
+                page = images[int_val-1]
+                img = np.array(page)
+
+                scale_percent = 20
+                width = int(img.shape[1] * scale_percent / 100)
+                height = int(img.shape[0] * scale_percent / 100)
+                dim = (width, height)
+
+                # resize image
+                resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+                st.image(resized, caption='Selected document')
+
+                export_path = os.path.join(os.path.abspath(os.getcwd()), "ocr_doc_to_process/")
+                out_file = export_path + str(name) + ".png"
+                cv2.imwrite(out_file, img)
+                docs = os.listdir(export_path)
+
+        else:
+            export_path = os.path.join(os.path.abspath(os.getcwd()), "ocr_doc_to_process/")
+            out_file = export_path + str(name) + ".png"
+            cv2.imwrite(out_file, image2)
+            docs = os.listdir(export_path)
+            st.text('Done!')
 
 # #######################################################################################################################
 #                                              # === PROCESS NEW FILE === #
