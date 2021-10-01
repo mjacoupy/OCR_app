061ea7f6bd6a2ba104155b79645603368743bfba
mjacoupy <maxime.jacoupy@gmail.com>
2021-10-01 10:27:04 +0200

    test

--- Search_Engine/PG-App.py
+++ Search_Engine/PG-App.py
@@ -2,7 +2,7 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-10-01 10:26:26
+# @Last Modified time: 2021-10-01 10:26:59
 
 
 # #######################################################################################################################
@@ -149,7 +149,7 @@ if analysis == "[1] Image Import":
             f = image.read()
             b = bytearray(f)
             #print b[0]
-        st.markdown(b[0])
+        st.markdown(b)
 
         # s3.Bucket(my_bucket).put_object(Key='test_image.png', Body=byte_im)
 
