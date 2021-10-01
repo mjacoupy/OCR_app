cc22a30ca9e2a7c8335c0db651c5b42e5a6cc297
mjacoupy <maxime.jacoupy@gmail.com>
2021-10-01 11:29:22 +0200

    test

--- Search_Engine/PG-App.py
+++ Search_Engine/PG-App.py
@@ -2,7 +2,7 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-10-01 11:27:21
+# @Last Modified time: 2021-10-01 11:29:17
 
 
 # #######################################################################################################################
@@ -133,7 +133,7 @@ if analysis == "[1] Image Import":
 
     if data is not None and button:
         export_path = os.path.join(os.path.abspath(os.getcwd()), "ocr_doc_to_process/")
-        out_file = export_path + name + ".png"
+        out_file = export_path + str(name) + ".png"
         cv2.imwrite(out_file, image2)
         docs = os.listdir(export_path)
 
