23f22cde0e4b151a584c5dba5c61d5deb99b3ff2
mjacoupy <maxime.jacoupy@gmail.com>
2021-10-01 10:13:34 +0200

    test

--- Search_Engine/PG-App.py
+++ Search_Engine/PG-App.py
@@ -2,7 +2,7 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-10-01 10:11:49
+# @Last Modified time: 2021-10-01 10:13:27
 
 
 # #######################################################################################################################
@@ -143,8 +143,9 @@ if analysis == "[1] Image Import":
         cv2.imwrite(out_file, image2)
         docs = os.listdir(export_path)
         st.markdown(docs)
-        data = open(out_file, 'rb')
-        s3.Bucket(my_bucket).put_object(Key='test_image.png', Body=data)
+        with open(out_file, 'rb') as f:
+            byte_im = f.read()
+        s3.Bucket(my_bucket).put_object(Key='test_image.png', Body=byte_im)
 # #######################################################################################################################
 #                                              # === PROCESS NEW FILE === #
 # #######################################################################################################################
