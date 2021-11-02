d64a623a1399d9ddc5964ed06fb0173d1efb4819
mjacoupy <maxime.jacoupy@gmail.com>
2021-11-02 17:38:16 +0100

    test image to s3

--- Search_Engine/OCRplus_app_demo.py
+++ Search_Engine/OCRplus_app_demo.py
@@ -2,7 +2,7 @@
 # @Author: mjacoupy
 # @Date:   2021-09-29 11:02:47
 # @Last Modified by:   mjacoupy
-# @Last Modified time: 2021-11-02 17:37:10
+# @Last Modified time: 2021-11-02 17:38:12
 
 
 # #######################################################################################################################
@@ -183,7 +183,8 @@ if analysis == "Import":
             export_path = os.path.join(os.path.abspath(os.getcwd()), "ocr_doc_to_process/")
             out_file = export_path + str(name) + "-" + str(int_val) + ".png"
             # cv2.imwrite(out_file, img)
-            img_to_s3(img, (name+'.png'))
+            n = name+'.png'
+            img_to_s3(img, str(n))
 
             str_text = extract_content_to_txt(img)
             out_file = str(name)+'.txt'
